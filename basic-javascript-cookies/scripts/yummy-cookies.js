3d317b00e599c848284cb9ff56de79dd751a45a0
Spencer lommel <spencerlommel@gmail.com>
2024-07-02 21:01:55 -0500

    added to cookies testing

--- basic-javascript-cookies/scripts/yummy-cookies.js
+++ basic-javascript-cookies/scripts/yummy-cookies.js
@@ -5,6 +5,17 @@ function getCookie(name) {
     if (parts.length === 2) return parts.pop().split(';').shift();
   }
 
+function randomHexColor() {
+    chars = "0123456789abcdef"
+    hexColor = "";
+    for (i=0; i < 6; i++) {
+        // https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
+        hexColor += chars.charAt(Math.floor(Math.random() * chars.length))
+    }
+
+    return hexColor
+}
+
 let sitevisits = document.cookie;
 
 window.addEventListener("load", (event) => {
@@ -13,7 +24,7 @@ window.addEventListener("load", (event) => {
     } else {
         document.cookie = "sitevisits=" +  (parseInt(getCookie("sitevisits")) + 1)
         document.getElementById("site-visits-amount").innerHTML = "Times you have visited this site: " + getCookie("sitevisits") + "!"
-
-        
+        document.getElementById("site-visits-amount").style = "color: #" + randomHexColor() + "; font-size: " + parseInt(getCookie("sitevisits")) + "px;"
+        document.getElementById("body").style = "background-color: #" + randomHexColor() + ";"
     }
   });
